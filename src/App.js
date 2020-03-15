d54c268fbeb22206cd5f447502c4c58529a9ef70
Nanda Gopal <ngopal253@gmail.com>
2020-03-15 12:36:38 +0530

    helmet added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Layout from './core/Layout';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
+  const head = () => (
+    <Helmet>
+      <meta charSet='utf-8' />
+      <title>MERN Stack</title>
+      <link rel='canonical' href='https://nandagopal.dev' />
+    </Helmet>
+  );
+
   return (
     <Layout>
+      {head()}
       <div className='col-md-6 offset-md-3 text-center'>
         <h1 className='p-5'>Authentication Boilerplate [MERN]</h1>
         <hr />
